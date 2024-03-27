7a84e46499001b1d11d18a39252e8e5840e4eb91
Dmitry Hrabrov <root@dexp.in>
2024-03-27 20:38:40 +0100

    Add modules

--- README.md
+++ README.md
@@ -1,39 +1,47 @@
-# jackett.dlm
-Download Station search module for Jackett (https://github.com/Jackett/Jackett)
+# Jackett-Indexers.dlm
+Download Station search modules for Jackett (https://github.com/Jackett/Jackett) - individual module per Jackett indexer.
+
+The goal is to have the results displaying a correct Site column. And getting the results faster - per indexer, not all the info at a time.
+
+![Synology Download Station Search BT results](/screenshots/Download-Station.png?raw=true "Synology Download Station Search BT results")
+
+The code for each module is a slight variation of the original code from [dimitrov-adrian/jackett.dlm](https://github.com/dimitrov-adrian/jackett.dlm)
 
 
 ## Installation
 
-1. Check - WORKING AND ACCESSIBLE JACKETT INSTANCE
+![Synology Download Station BT Search configuration](/screenshots/BT-Search-Settings.png?raw=true "Synology Download Station BT Search configuration")
 
-1. Download latest [jackett.dlm](https://github.com/dimitrov-adrian/jackett.dlm/releases/download/1.0.2/jackett.dlm) module
+1. Check - WORKING AND ACCESSIBLE JACKETT INSTANCE. Configure some Indexers there. For example: AniLibria
 
-2. Install and enable to your Download Station -> Settings -> BT Search -> Add
+2. Download respective modules from [dlm folder](/dlm) for each indexer you'd like to use in Download Station. Example: [dlm/Anilibria-Jackett.dlm](/dlm/Anilibria-Jackett.dlm)
 
-3. Enable Jackett and edit settings.
+3. Install and enable to your Download Station -> Settings -> BT Search -> Add
 
-4. Use your jackett instance **host as username**, and **api key as password**
+4. Enable Jackett and edit settings.
+
+5. Use your jackett instance **host as username**, and **api key as password**
 > Example: If your Jackett instance is accessible via *http://192.168.0.1:9117/UI/Dashboard*,
 > then the username is **192.168.0.1:9117**
 
-5. Click OK
+6. It is a good idea to click "Verify" after setting up host and API key.
+
+7. Click OK
+
+![Synology Download Station Search BT results](/screenshots/Edit-AniLibria.png?raw=true "Synology Download Station Search BT results")
 
 
 ## Compatibility
 
-Tested on Synology DSM 6.1-6.2
-(I just have no older version to test)
+Tested on Synology DSM 6.1-6.2 and 7.2.1
 
 
 ## Building
 
-```bash
-git clone https://github.com/dimitrov-adrian/jackett.dlm.git
-cd jackett.dlm.git
-make
-```
+(optional) Download and replace `indexers.json` from your Jackett instance: http://your_jackett_server/api/v2.0/indexers
 
-## Testing
+Call the `generate.py` - it will create/update the [dlm](/dlm) folder for you.
+
 ```bash
-make tests ARGS="<hostname> <apikey>"
-````
+python3 generate.py
+```
---
+++ generate.py
@@ -0,0 +1,175 @@
+#!/usr/bin/python3
+
+import json, tarfile, os, io
+
+print("Get your indexers.json from: http://your_jackett_server/api/v2.0/indexers")
+
+with open('indexers.json') as json_file:
+    indexers = json.load(json_file)
+
+    if not os.path.isdir("dlm"):
+        os.mkdir("dlm") 
+
+    for ind in indexers:
+        cid = ind["id"]
+        gid = cid.capitalize()
+        name = ind["name"]
+        site = ind["site_link"]
+        className = f"SynoDLMSearch{gid}Jackett"
+
+        # Hacks
+        page = "$child->guid"
+        if cid == "anilibria":
+            page = "$child->comments"
+
+        info = {
+            "name": cid,
+            "displayname": name,
+            "description": f"{name} via Jackett",
+            "version": "1.0",
+            "accountsupport": True,
+            "site": site,
+            "module": "search.php",
+            "type": "search",
+            "class": className
+        }
+        infoStr = json.dumps(info, indent=4)
+
+        tarInfo = tarfile.TarInfo('INFO')
+        tarInfo.size = len(infoStr)
+
+        searchCode = """<?php
+
+/**
+ * Synology search class
+ */
+class """ + className + """
+{
+  public $debug = FALSE;
+  private $qurl = 'http://<host>/api/v2.0/indexers/""" + cid + """/results/torznab/api?apikey=<apikey>&t=search&cat=&q=<query>';
+
+  /**
+   * synology hook
+   *
+   * @param $curl
+   * @param $query
+   * @param $username
+   * @param $password
+   */
+  public function prepare($curl, $query, $username = '', $password = '')
+  {
+
+    // Strip protocols from the setting.
+    $username = strtr($username, [
+      'http://' => '',
+      'https://' => '',
+    ]);
+
+    $url = strtr($this->qurl, [
+      '<host>' => trim($username, ' /'),
+      '<apikey>' => urlencode(trim($password)),
+      '<query>' => urlencode(trim($query)),
+    ]);
+
+    if ($this->debug) {
+      error_log("REQUEST: {$url}");
+    }
+
+    // Setup the $curl handler.
+    curl_setopt($curl, CURLOPT_URL, $url);
+  }
+
+  /**
+   * synology hook
+   *
+   * @param $plugin
+   * @param $response
+   *
+   * @return int
+   */
+  public function parse($plugin, $response)
+  {
+    $count = 0;
+
+    $xml = simplexml_load_string(trim($response), 'SimpleXMLElement');
+
+    if (empty($xml->channel)) {
+      return $count;
+    }
+    
+    foreach ($xml->channel->item as $child) {
+
+      $peers = $child->xpath('torznab:attr[@name="peers"]')
+        ? (int)$child->xpath('torznab:attr[@name="peers"]')[0]['value']
+        : 0;
+
+      $seeders = $child->xpath('torznab:attr[@name="seeders"]')
+        ? (int)$child->xpath('torznab:attr[@name="seeders"]')[0]['value']
+        : 0;
+
+      $categories = [];
+      foreach ($child->xpath('torznab:attr[@name="category"]') as $category) {
+        $categories[] = $category['value'];
+      }
+
+      $leechs = $peers ? $peers - $seeders : 0;
+      $title = urldecode((string) $child->title);
+      $download = (string) $child->link;
+      $size = (double) $child->size;
+      $datetime = date('Y-m-d H:i:s', strtotime($child->pubDate));
+      $page = (string) """ + page + """;
+      $hash = md5($count . $download);
+
+      $plugin->addResult($title, $download, $size, $datetime, $page, $hash, $seeders, $leechs, array_shift($categories));
+
+      $count++;
+    }
+
+    return $count;
+  }
+
+  /**
+   * synology hook
+   *
+   * @param $username
+   * @param $password
+   *
+   * @return bool
+   */
+  public function VerifyAccount($username, $password)
+  {
+
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+      CURLOPT_RETURNTRANSFER => TRUE,
+      CURLOPT_SSL_VERIFYHOST => FALSE,
+      CURLOPT_SSL_VERIFYPEER => FALSE,
+      CURLOPT_TIMEOUT => 20,
+      CURLOPT_FOLLOWLOCATION => TRUE,
+      CURLOPT_USERAGENT => 'Mozilla/4.0 (compatible; MSIE 6.1; Windows XP)',
+    ]);
+
+    $this->prepare($curl, '', $username, $password);
+
+    $result = curl_exec($curl);
+    $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+    curl_close($curl);
+
+    if (strpos($result, 'Invalid API Key') !== false) {
+      return false;
+    }
+
+    return (int) $httpcode == 200;
+  }
+
+}
+"""
+        tarSearch = tarfile.TarInfo('search.php')
+        tarSearch.size = len(searchCode)
+
+        with tarfile.open(f'dlm/{gid}-Jackett.dlm', 'w:gz') as tar:
+            tar.addfile(tarInfo, io.BytesIO(infoStr.encode('utf8')))
+            tar.addfile(tarSearch, io.BytesIO(searchCode.encode('utf8')))
+
+        print(f"{cid} - {name}")
